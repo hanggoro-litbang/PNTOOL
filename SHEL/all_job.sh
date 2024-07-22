61a5920fc1b19fd1e44d7ebe80c7eff02bf85a33
hanggoro <125229690+hanggoro-litbang@users.noreply.github.com>
2024-07-22 13:47:13 +0700

    Update all_job.sh

--- SHEL/all_job.sh
+++ SHEL/all_job.sh
@@ -3,15 +3,19 @@
 #######################################################
 # user modification
 #######################################################
- start_time=2021011100
- end_time=2021011200
- run_days=1
+ #start_time=2021011100
+ #end_time=2021011200
+ #run_days=1
+start_time=$(date +"%Y%m%d")
+run_days=1
+end_time=$(date +"%Y%m%d" -d "+$run_days days")
+cc=00
 # for 3dvar start_da = start_time - 1, end_da=satrt_time + 1 
  start_da=2021011023
  end_da=2021011101
  bdy_int=6
  case=DOM1
- wuudir=~/UNTOOL
+ wuudir=~/PNTOOL
  num_proc=1
  domain=1
 #
@@ -35,7 +39,7 @@ shelldir="${wuudir}/SHEL"
  if ($run_name) ; then
    echo "main_shell start: prepare namelist"
 #   bash ./run0_namelist_2dom.sh $start_time $end_time $run_days $bdy_int
-   bash ./run0_namelist.sh $start_time $end_time $run_days $bdy_int $wuudir $case $start_da $end_da $domain
+   bash ./run0_namelist.sh $start_time$cc $end_time$cc $run_days $bdy_int $wuudir $case $start_da $end_da $domain
    echo "main_shell end: prepare namelist"
  else
    echo "skip namelist modification"
@@ -47,7 +51,7 @@ shelldir="${wuudir}/SHEL"
  if ($run_gfs) ; then
    echo "main_shell start: gfs download"
    bash ./run1_gfs_prepare.sh $run_days $bdy_int
-   bash ./run1_gfs.sh $start_time $wuudir
+   bash ./run1_gfs.sh $start_time$cc $wuudir
    echo "main_shell end: gfs download"
  else
    echo "skip to download GFS data"
@@ -58,7 +62,7 @@ shelldir="${wuudir}/SHEL"
 #----------------------------------------------
  if ($run_wps) ; then
    echo "main_shell start: wps"
-   bash ./run2_wps.sh $start_time $wuudir
+   bash ./run2_wps.sh $start_time$cc $wuudir
    echo "main_shell end: wps"
  else
    echo "skip to run WPS(ungrib, metgrid)"
@@ -78,7 +82,7 @@ shelldir="${wuudir}/SHEL"
 #----------------------------------------------
  if ($run_wrfda) ; then
    echo "main_shell start: wrfda 3dvar"
-   bash ./run4_da.sh $start_time $wuudir $domain
+   bash ./run4_da.sh $start_time$cc $wuudir $domain
    echo "main_shell end: wrfda 3dvar"
  else
    echo "skip to run real"
@@ -99,7 +103,7 @@ shelldir="${wuudir}/SHEL"
 #----------------------------------------------
  if ($run_ncl) ; then
    echo "main_shell start: ncl"
-   bash ./run6_ncl.sh $start_time $domain $wuudir
+   bash ./run6_ncl.sh $start_time$cc $domain $wuudir
    echo "main_shell end: ncl"
  else
    echo "skip to run ncl"
